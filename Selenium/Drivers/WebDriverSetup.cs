dfbb628eb4a785098f95aab8447d31430942f071
Suresh Nagalla <suresh_nagalla@epam.com>
2025-02-19 21:32:03 +0530

    Add project files.

---
+++ Selenium/Drivers/WebDriverSetup.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SwagLabProject.Selenium.Drivers
+{
+    public class WebDriverSetup
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ Selenium/TestSuite/LoginTests.cs
@@ -0,0 +1,18 @@
+﻿using SwagLabProject.Selenium.Actions;
+using SwagLabProject.Selenium.Drivers;
+
+namespace SwagLabProject.Selenium.TestSuite
+{
+    public class LoginTests : WebDriverSetup
+    {
+        [Test]
+        public void Login_WithValidCredentials_ShouldBeSuccessful()
+        {
+            var loginActions = new LoginActions(driver);
+            loginActions.Login("standard_user", "secret_sauce");
+
+            var loginPage = new SwagLabProject.Selenium.Pages.LoginPage(driver);
+            Assert.That(driver.Url.Contains("inventory"), "Login failed!");
+        }
+    }
+}
