c56b4415262e31cb6d8bf5eddee03707f892866c
suresh-nagalla <suri.nagalla@gmail.com>
2025-02-21 13:47:33 +0530

    Create PlaywrightCode/Actions/LoginActions.cs

---
+++ PlaywrightCode/Actions/LoginActions.cs
@@ -0,0 +1,19 @@
+using Microsoft.Playwright;
+using SwagLabProject.PlaywrightCode.Pages;
+
+namespace SwagLabProject.PlaywrightCode.Actions
+{
+    public class LoginActions
+    {
+        private readonly LoginPage _loginPage;
+
+        public LoginActions(IPage page) => _loginPage = new LoginPage(page);
+
+        public async Task LoginAsync(string username, string password)
+        {
+            await _loginPage.EnterUsernameAsync(username);
+            await _loginPage.EnterPasswordAsync(password);
+            await _loginPage.ClickLoginAsync();
+        }
+    }
+}
