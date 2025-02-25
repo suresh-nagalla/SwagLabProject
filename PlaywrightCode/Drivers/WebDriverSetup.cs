55f1bc5e77287efd3f3da9642c77c77c877f09a8
suresh-nagalla <suri.nagalla@gmail.com>
2025-02-25 22:26:24 +0530

    Create PlaywrightCode/Drivers/WebDriverSetup.cs

---
+++ PlaywrightCode/Drivers/WebDriverSetup.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using Playwright;
+
+namespace SwagLabProject.PlaywrightCode.Drivers
+{
+    public class WebDriverSetup
+    {
+        protected IBrowser _browser;
+        protected IBrowserContext _context;
+        protected IPage _page;
+
+        [SetUp]
+        public async Task SetupAsync()
+        {
+            var playwright = await Playwright.CreateAsync();
+            _browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+            _context = await _browser.NewContextAsync();
+            _page = await _context.NewPageAsync();
+            await _page.GotoAsync("https://www.saucedemo.com/");
+        }
+
+        [TearDown]
+        public async Task TearDownAsync()
+        {
+            await _context.CloseAsync();
+            await _browser.CloseAsync();
+        }
+    }
+}
