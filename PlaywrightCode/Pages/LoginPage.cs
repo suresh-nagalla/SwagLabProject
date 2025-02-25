3eec3e0c7106fcacfa5fba16a1b755f86181a412
suresh-nagalla <suri.nagalla@gmail.com>
2025-02-25 21:54:14 +0530

    Create PlaywrightCode/Pages/LoginPage.cs

---
+++ PlaywrightCode/Pages/LoginPage.cs
@@ -0,0 +1,41 @@
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace SwagLabProject.Playwright.Pages
+{
+    public class LoginPage
+    {
+        private readonly IPage _page;
+        private readonly ILocator _usernameField;
+        private readonly ILocator _passwordField;
+        private readonly ILocator _loginButton;
+
+        public LoginPage(IPage page)
+        {
+            _page = page;
+            _usernameField = _page.Locator("#user-name");
+            _passwordField = _page.Locator("#password");
+            _loginButton = _page.Locator("#login-button");
+        }
+
+        public async Task EnterUsernameAsync(string username)
+        {
+            await _usernameField.FillAsync(username);
+        }
+
+        public async Task EnterPasswordAsync(string password)
+        {
+            await _passwordField.FillAsync(password);
+        }
+
+        public async Task ClickLoginAsync()
+        {
+            await _loginButton.ClickAsync();
+        }
+
+        public async Task<bool> IsOnLoginPageAsync()
+        {
+            return (await _page.UrlAsync()).Contains("saucedemo");
+        }
+    }
+}
