ae532b266f42e67fcd5ff6ff44f9ed31ed8cfc6d
suresh-nagalla <suri.nagalla@gmail.com>
2025-02-26 09:21:22 +0530

    Create PlaywrightCode/TestSuite/LoginTests.cs

---
+++ PlaywrightCode/TestSuite/LoginTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using SwagLabProject.PlaywrightCode.Actions;
+using SwagLabProject.PlaywrightCode.Drivers;
+using System.Threading.Tasks;
+
+namespace SwagLabProject.PlaywrightCode.TestSuite
+{
+    public class LoginTests : WebDriverSetup
+    {
+        [Test]
+        public async Task Login_WithValidCredentials_ShouldBeSuccessfulAsync()
+        {
+            var loginActions = new LoginActions(Page);
+            await loginActions.LoginAsync("standard_user", "secret_sauce");
+
+            var loginPage = new SwagLabProject.PlaywrightCode.Pages.LoginPage(Page);
+            Assert.That((await Page.Url).Contains("inventory"), "Login failed!");
+        }
+    }
+}
